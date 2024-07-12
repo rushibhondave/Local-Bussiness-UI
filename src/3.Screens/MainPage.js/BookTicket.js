d982112566dc76c1d57c1863ee2246df67004d09
rushibhondave <rushikeshbhondav99@gmail.com>
2024-07-13 02:16:05 +0530

    Main Page work in progress

--- src/3.Screens/MainPage.js/BookTicket.js
+++ src/3.Screens/MainPage.js/BookTicket.js
@@ -12,19 +12,22 @@ function BookTicket() {
 
         <div className="ticket_Card">
         <div className="WordCenter">
-        <h2 className="wordColor ">Choose Your Ticket</h2> </div>
+        <h2 className="wordColor ">Choose Your Route</h2> </div>
           <div className="ticket_block">
          
             <div className="book">
-              <select name="" id="" className="selction">
-                <option value="English">English</option>
-                <option value="Hindi">Hindi</option>
-                <option value="Marathi">Marathi</option>
+              <select name="" id="" className="selction wordColor">
+              <option value="">Pickup point </option>
+                <option value="Shivajinagar">Shivajinagar</option>
+                <option value="Pune Station">Pune Station</option>
+                <option value="Swargate ">Swargate </option>
+                <option value="Katraj  ">Katraj  </option>
               </select>
-              <select name="" id="" className="selction">
-                <option value="English">English</option>
-                <option value="Hindi">Hindi</option>
-                <option value="Marathi">Marathi</option>
+              <select name="" id="" className="selction wordColor" >
+              <option value="">dropping point </option>
+                <option value="Pune Station">Pune Station</option>
+                <option value="Shivajinagar">Shivajinagar</option>
+                <option value="Swargate  ">Swargate  </option>
               </select>
               <div className="btnC">
               <button className="btn button">Find Tickets</button>
