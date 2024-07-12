ec56300df305652416c0721c91fba542cfb85013
rushibhondave <rushikeshbhondav99@gmail.com>
2024-07-13 01:49:55 +0530

    Main Page work in progress

--- src/3.Screens/MainPage.js/BookTicket.js
+++ src/3.Screens/MainPage.js/BookTicket.js
@@ -1,19 +1,48 @@
-import React from 'react'
+import React from "react";
 import "../../Style/MainPage.css";
+import BusImg from "../../Img/BusImg.png";
 function BookTicket() {
   return (
-    <div>
-        <div className="container">
-                <div className="ticket_info">
-               <h1>Get Your Ticket Online, Easy and Safely</h1>
-               <button className='btn  button'> GET TICKET NOW </button>
-                </div>
-                <div className="ticket_buy">
-                  
-                  </div>
+    <div className="container">
+      <div className="wrapper">
+        <div className="ticket_info">
+          <h1 className="h1 wordColor">Get Your Ticket Online, Easy and Safely</h1>
+          <button className="btn btnU"> GET TICKET NOW </button>
         </div>
+
+        <div className="ticket_Card">
+
+          <div className="ticket_block">
+            <div className="book">
+              <select name="" id="" className="selction">
+                <option value="English">English</option>
+                <option value="Hindi">Hindi</option>
+                <option value="Marathi">Marathi</option>
+              </select>
+              <select name="" id="" className="selction">
+                <option value="English">English</option>
+                <option value="Hindi">Hindi</option>
+                <option value="Marathi">Marathi</option>
+              </select>
+              <div className="btnC">
+              <button className="btn button">Find Tickets</button>
+            </div>
+            </div>
+           
+          </div>
+        </div>
+      </div>
+
+      <div className="build_img">
+
+        <marquee behavior="" direction="">
+          <img src={BusImg} alt="" />
+        </marquee>
+      </div>
     </div>
-  )
+
+    
+  );
 }
 
-export default BookTicket
+export default BookTicket;
